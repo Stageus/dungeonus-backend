35a4bddc9a2770a41c36445f958572f803b3fe46
shinjeongmin <sin37minduke@naver.com>
2022-01-25 17:47:42 +0000

    Create account router basic frame Need connect postgres client and installing pg to require pg client

---
+++ router/account.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const client = require("./connectClient.js");
+
+router.post("/login", (req,res) =>{
+
+});
+
+router.post("/logout", (req,res)=>{
+
+});
+
+router.delete("/", (req,res) =>{
+
+});
+
+router.put("/", (req,res)=>{
+
+});
+
+router.post("/", (req,res)=>{
+
+});
+
+router.get("/total", () =>{
+    
+});
+
+module.exports = router;
--- server_sects/server_sect1.js
+++ server_sects/server_sect1.js
@@ -4,4 +4,7 @@ const app = express();
 const main = require("../router/main.js");
 app.use("/main", main);
 
+// const account = require("../router/account.js");
+// app.use("/account", account);
+
 module.exports = app;
